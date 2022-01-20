af563bd7c23531ac4e4d0d0d788d54c9ee7cd85f
Pedro Correia <pedrogrcorreia@gmail.com>
2022-01-20 04:31:08 +0000

    added register on laboratorytesters

--- MultiLabs/MultiLabs/Controllers/LaboratoryTestersController.cs
+++ MultiLabs/MultiLabs/Controllers/LaboratoryTestersController.cs
@@ -5,19 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MultiLabs.Data;
 using MultiLabs.Models;
+using MultiLabs.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace MultiLabs.Controllers
 {
     public class LaboratoryTestersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
 
-        public LaboratoryTestersController(ApplicationDbContext context)
+        public LaboratoryTestersController(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: LaboratoryTesters
@@ -159,6 +170,34 @@ namespace MultiLabs.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Register(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Register")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register()
+        {
+            //TODO deixar o manager criar o tester com o mail que desejar
+            // fazer isso na vista ou seja adicionar um campo para ele meter o 
+            // email
+            var id = _context.Users.OrderBy(l => l.Id).LastAsync().Id + 1;
+
+            var user = new User { UserName = new string("tester" + id + "@gmail.com"), Email = new string("manager" + id + "@gmail.com") };
+            var result = await _userManager.CreateAsync(user, "123Vv#");
+            if (result.Succeeded)
+            {
+                int roleId = 3;
+                var role = Roles.RolesUtils.All;//RegistSelectList.FirstOrDefault(s => s.Value == roleId.ToString());
+                await (_userManager.AddToRoleAsync(user, "LabTester"));
+                ViewBag.Success = "Tester with email " + "tester" + id + "@gmail.com" + " created successfully!";
+            }
+            return View();
+        }
+
 
         private bool LaboratoryTestersExists(int id)
         {
